718ca6eaacb3964fef75d5f5db384f68d0e765cf
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-27 14:02:29 +0100

    Created method that returns string of employee names

--- AcademiaDB/Repositories/EmployeeRepository.cs
+++ AcademiaDB/Repositories/EmployeeRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace AcademiaDB.Repositories;
+﻿using AcademiaDB.Data;
+
+namespace AcademiaDB.Repositories;
 
 public class EmployeeRepository
 {
-    
+    private AcademiaContext _context;
+
+    public EmployeeRepository(AcademiaContext context)
+    {
+        _context = context;
+    }
+
+    public string GetEmployeeNames()
+    {
+        var employeeNames = _context.Employees
+            .Select(s => $"{s.EmployeeFirstName} {s.EmployeeLastName}")
+            .ToList();
+
+        return string.Join("\n", employeeNames);
+    }
 }
