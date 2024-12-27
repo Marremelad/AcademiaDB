5895da7577b30428dba59aeafb80a59a13df6872
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-27 15:30:42 +0100

    Added method that returns students with the top grades

--- AcademiaDB/Repositories/ViewRepository.cs
+++ AcademiaDB/Repositories/ViewRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace AcademiaDB.Repositories;
+﻿using AcademiaDB.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AcademiaDB.Repositories;
 
 public class ViewRepository
 {
-    
+    private AcademiaContext _context;
+
+    public ViewRepository(AcademiaContext context)
+    {
+        _context = context;
+    }
+
+    public string GetTopGrades()
+    {
+        var topGrades = _context.TopGrades
+            .Select(s => $"{s.Name} {s.Grade}")
+            .ToList();
+
+        var result = string.Join("\n", topGrades);
+        return result;
+    }
 }
