d07169d29f9a540afeaa846926b7bd0d8a7b5e84
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-27 14:29:56 +0100

    Added view for top grades

---
+++ AcademiaDB/Models/TopGrade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcademiaDB.Models;
+
+public partial class TopGrade
+{
+    public string Name { get; set; } = null!;
+
+    public string? Grade { get; set; }
+}
