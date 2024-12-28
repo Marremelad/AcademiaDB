6291d7962b21a4d2ed0d9331ddda96ff7dc46256
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-28 16:45:28 +0100

    Added dependency to class and call method in switch statement

--- AcademiaDB/UserInterface/Menus/StudentMenu.cs
+++ AcademiaDB/UserInterface/Menus/StudentMenu.cs
@@ -1,6 +1,27 @@
-﻿namespace AcademiaDB.UserInterface.Menus;
+﻿using AcademiaDB.Repositories;
+using AcademiaDB.UserInterface.MenuOptions;
+using AcademiaDB.UserInterface.SelectionPrompts;
+
+namespace AcademiaDB.UserInterface.Menus;
 
 public class StudentMenu
 {
-    
+    private StudentRepository _studentRepository;
+
+    public StudentMenu(StudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
+    public void DisplayStudentMenu()
+    {
+        var selection = Prompt.DisplaySingleChoicePrompt("Select an option", MenuText.StudentMenuText);
+
+        switch (selection)
+        {
+            case MenuText.Options.AllStudents:
+                Console.WriteLine(_studentRepository.DisplayStudentsWithClasses(s => s.StudentId, true));
+                break;
+        }
+    }
 }
