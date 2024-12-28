2a86b40b9ad53a36af1f695b5d5b8ae1ad555d9c
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-28 13:02:56 +0100

    Moved IsValidCombination and ApplyOptions to PromptHelper class

--- AcademiaDB/Helpers/PromptHelper.cs
+++ AcademiaDB/Helpers/PromptHelper.cs
@@ -1,6 +1,55 @@
-﻿namespace AcademiaDB.Helpers;
+﻿using System.Linq.Expressions;
+using AcademiaDB.Models;
+using AcademiaDB.UserInterface.MenuOptions;
 
-public class PromptHelper
+namespace AcademiaDB.Helpers;
+
+public static class PromptHelper
 {
+    // Check if the chosen combination of options is valid.
+    public static bool IsValidCombination(List<MenuText.Options> choice)
+    {
+        
+        // List containing lists of valid combinations.
+        List<List<MenuText.Options>> validCombinations =
+        [
+            new() { MenuText.Options.SortByFirstName, MenuText.Options.OrderByDescending },
+            new() { MenuText.Options.SortByLastName, MenuText.Options.OrderByDescending },
+            new() { MenuText.Options.SortByFirstName, MenuText.Options.OrderByAscending },
+            new() { MenuText.Options.SortByLastName, MenuText.Options.OrderByAscending }
+        ];
+        
+        return validCombinations.Any(vc =>
+            vc.SequenceEqual(choice)); 
+    }
     
+    // Applies the combination of options and calls the method to retrieve student information.
+    public static Tuple<Expression<Func<Student, string>>, bool> ApplyOptions(List<MenuText.Options> choice)
+    {
+        Expression<Func<Student, string>> sortBy;
+        bool orderBy;
+        
+        if ((int)choice[0] == 13 && (int)choice[1] == 15)
+        {
+            sortBy = s => s.StudentFirstName;
+            orderBy = false; // Order by Descending.
+        }
+        else if ((int)choice[0] == 14 && (int)choice[1] == 15)
+        {
+            sortBy = s => s.StudentLastName;
+            orderBy = false; // Order by Descending.
+        }
+        else if((int)choice[0] == 13 && (int)choice[1] == 16)
+        {
+            sortBy = s => s.StudentFirstName;
+            orderBy = true; // Order by Ascending.
+        }
+        else
+        {
+            sortBy = s => s.StudentLastName;
+            orderBy = true; // Order by Ascending.
+        }
+
+        return new Tuple<Expression<Func<Student, string>>, bool>(sortBy, orderBy);
+    }
 }
