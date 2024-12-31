e5531fe038eba50482b0825f3d606e56de9a9817
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-12-31 16:25:59 +0100

    Added methods to assign or update a students grade for each course

--- AcademiaDB/Repositories/CourseEnrolmentRepository.cs
+++ AcademiaDB/Repositories/CourseEnrolmentRepository.cs
@@ -1,5 +1,7 @@
-﻿using AcademiaDB.Data;
+﻿using System.ComponentModel;
+using AcademiaDB.Data;
 using AcademiaDB.Models;
+using AcademiaDB.UserInterface.MenuOptions;
 using Microsoft.EntityFrameworkCore;
 using Prompt = AcademiaDB.UserInterface.SelectionPrompts.Prompt;
 
@@ -16,7 +18,7 @@ public class CourseEnrolmentRepository
     
     // Displays a list of all courses that a student has been enrolled into.
     // User can then select one of the courses to see the enrolment information.
-    public string GetStudentCourseEnrolments(int studentId)
+    public (string, int) GetStudentCourseEnrolments(int studentId)
     {
         var courseEnrolments = _context.CourseEnrolments
             .Where(ce => ce.StudentIdFk == studentId)
@@ -27,7 +29,7 @@ public class CourseEnrolmentRepository
 
         var courseEnrolmentObject = (CourseEnrolment)selection;
 
-        return GetInformationString(courseEnrolmentObject);
+        return (GetInformationString(courseEnrolmentObject), courseEnrolmentObject.EnrolmentId);
     }
     
     // Returns a string with the chosen course enrolment information.
@@ -43,7 +45,7 @@ public class CourseEnrolmentRepository
 
         Console.Clear();
         
-        return $"Enrolment information\n" +
+        return $"Enrolment information\n\n" +
                $"Enrolment ID: {courseEnrolment.EnrolmentId}\n" +
                $"Course Name: {courseEnrolment.CourseIdFkNavigation.CourseName}\n" +
                $"Student: {courseEnrolment.StudentIdFkNavigation.StudentFirstName} " +
@@ -52,5 +54,75 @@ public class CourseEnrolmentRepository
                $"Grade set by: {courseEnrolment.GradeSetterFkNavigation?.EmployeeFirstName} " +
                $"{courseEnrolment.GradeSetterFkNavigation?.EmployeeLastName}\n" +
                $"Grading date: {courseEnrolment.GradingDate}";
+    }
+    
+    public void UpdateGradeOptions(int enrolmentId)
+    {
+        var selection = Prompt.DisplaySingleChoicePrompt("", MenuText.UpdateGradeText);
+
+        switch (selection)
+        {
+            case MenuText.Options.UpdateGrade:
+                SelectValidGrade(enrolmentId);
+                break;
+            
+            case MenuText.Options.Exit:
+                return;
+        }
+    }
+
+    private void SelectValidGrade(int enrolmentId)
+    {
+        var grade = _context.CourseEnrolments
+            .Where(ce => ce.EnrolmentId == enrolmentId)
+            .Select(ce => ce.Grade)
+            .SingleOrDefault();
+        
+        List<string> validGrades =
+        [
+            "A", "B", "C", "D", "E", "F", "Exit"
+        ];
+        
+        if (grade != null) validGrades.Remove(grade);
+
+        Console.Clear();
+        
+        var selection = Prompt.DisplaySingleChoicePrompt("Select a grade", validGrades);
+
+        if ((string)selection == "Exit") return;
+
+        var stringObject = (string)selection;
+        
+        UpdateCourseGrade(enrolmentId, stringObject);
+    }
+    
+    private void UpdateCourseGrade(int enrolmentId, string grade)
+    {
+        var courseEnrolment = _context.CourseEnrolments
+            .SingleOrDefault(ce => ce.EnrolmentId == enrolmentId);
+
+        if (courseEnrolment == null)
+        {
+            Console.WriteLine("Course enrolment not found...");
+            return;
+        }
+
+        try
+        {
+            courseEnrolment.Grade = grade;
+            // var currentDate = DateTime.Now;
+            courseEnrolment.GradingDate = DateOnly.FromDateTime(DateTime.Today);
+            _context.SaveChanges();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Something went wrong while attempting to update a grade.\n" +
+                              "Make sure that the grade is a valid character.('A', 'B', 'C', 'D', 'E', 'F')\n" +
+                              "Rolling back changes...");
+            return;
+        }
+
+        Console.Clear();
+        Console.WriteLine("Grade updated successfully.");
     }
 }
