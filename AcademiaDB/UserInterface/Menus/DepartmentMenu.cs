6595598b72915d1174d500176156d66bafeb6515
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-01-01 17:10:38 +0100

    Added switch case option

--- AcademiaDB/UserInterface/Menus/DepartmentMenu.cs
+++ AcademiaDB/UserInterface/Menus/DepartmentMenu.cs
@@ -1,4 +1,6 @@
 ﻿using AcademiaDB.Repositories;
+using AcademiaDB.UserInterface.MenuOptions;
+using AcademiaDB.UserInterface.SelectionPrompts;
 
 namespace AcademiaDB.UserInterface.Menus;
 
@@ -10,6 +12,17 @@ public class DepartmentMenu
     {
         _departmentRepository = departmentRepository;
     }
-    
-    
+
+    public void DisplayDepartmentMenu()
+    {
+        var selection = Prompt.DisplaySingleChoicePrompt("Select an option",
+            MenuText.DepartmentMenuText);
+
+        switch (selection)
+        {
+            case MenuText.Options.AllDepartments:
+                _departmentRepository.GetDepartmentInformation(_departmentRepository.GetDepartments());
+                break;
+        }
+    }
 }
