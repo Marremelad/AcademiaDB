54b98e7e783ba9baaca6a8a431d98e970768a0d6
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-01-01 14:42:17 +0100

    Override ToString method

--- AcademiaDB/Models/Class.cs
+++ AcademiaDB/Models/Class.cs
@@ -14,4 +14,10 @@ public partial class Class
     public virtual Employee EmployeeIdFkNavigation { get; set; } = null!;
 
     public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+
+    // Returns a string with the name of the class.
+    public override string ToString()
+    {
+        return $"{ClassName}";
+    }
 }
