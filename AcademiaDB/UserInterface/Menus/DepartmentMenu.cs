e70e9d0750e825cd94d0ebfef5ed0ea06d1d93f8
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-01-01 16:39:49 +0100

    Added dependency to class

--- AcademiaDB/UserInterface/Menus/DepartmentMenu.cs
+++ AcademiaDB/UserInterface/Menus/DepartmentMenu.cs
@@ -1,6 +1,15 @@
-﻿namespace AcademiaDB.UserInterface.Menus;
+﻿using AcademiaDB.Repositories;
+
+namespace AcademiaDB.UserInterface.Menus;
 
 public class DepartmentMenu
 {
+    private DepartmentRepository _departmentRepository;
+
+    public DepartmentMenu(DepartmentRepository departmentRepository)
+    {
+        _departmentRepository = departmentRepository;
+    }
+    
     
 }
