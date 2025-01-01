10ae23baa7971afc4141304d31b6839d0049db2b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-01-01 15:02:36 +0100

    Added dependencies to class

--- AcademiaDB/UserInterface/Menus/ClassMenu.cs
+++ AcademiaDB/UserInterface/Menus/ClassMenu.cs
@@ -1,6 +1,22 @@
-﻿namespace AcademiaDB.UserInterface.Menus;
+﻿using AcademiaDB.Data;
+using AcademiaDB.Helpers;
+using AcademiaDB.Repositories;
+using AcademiaDB.UserInterface.MenuOptions;
+using AcademiaDB.UserInterface.SelectionPrompts;
+
+namespace AcademiaDB.UserInterface.Menus;
 
 public class ClassMenu
 {
-    
+    private ClassRepository _classRepository;
+
+    public ClassMenu(ClassRepository classRepository)
+    {
+        _classRepository = classRepository;
+    }
+
+    public void DisplayClassMenu()
+    {
+        UserInput.GetClassId("Select a class to see its students");
+    }
 }
