155cf85476c9c9aad0ff239dd5f84406955f7516
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-01-01 14:41:01 +0100

    Added method that returns a list of Class objects

--- AcademiaDB/Repositories/ClassRepository.cs
+++ AcademiaDB/Repositories/ClassRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace AcademiaDB.Repositories;
+﻿using AcademiaDB.Data;
+using AcademiaDB.Models;
+using Spectre.Console;
+
+namespace AcademiaDB.Repositories;
 
 public class ClassRepository
 {
-    
+    private AcademiaContext _context;
+
+    public ClassRepository(AcademiaContext context)
+    {
+        _context = context;
+    }
+
+    public List<Class> GetClasses()
+    {
+        var classes = _context.Classes
+            .ToList();
+
+        return classes;
+    }
 }
